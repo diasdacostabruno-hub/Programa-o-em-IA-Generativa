0b3645364581143482b06a866836a77e78eceac6
diasdacostabruno-hub <diasdacostabruno@gmail.com>
2026-05-08 17:57:26 -0300

    Add files via upload

---
+++ meuport.py
@@ -0,0 +1,15 @@
+# CRIE SUA APLICAÇÃO, COMO SE FOSSE O SEU PORTFÓLIO PESSOAL.
+import streamlit as st
+st.title('Este sou Eu')
+
+
+# COLOQUE NOME
+st.text_input('Nome:')
+# CURIOSIDADES
+st.text_input('Meus hobbis')
+st.number_input('Minha idade:')
+st.number_input('Minha altura:')
+# UMA IMAGEM QUE TE DEFINE
+st.image('https://png.pngtree.com/thumb_back/fh260/background/20240716/pngtree-d-cat-as-video-game-live-stream-gamer-use-pc-computer-image_16004832.jpg')
+
+# O SEU LINK DO GITHUB
